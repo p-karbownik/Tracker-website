f40751cbe7607fbfbf49618376e688517d38e9b4
aksek <kosekaniela@gmail.com>
2021-05-09 09:13:39 +0200

    add new website window

---
+++ src/new-website.component.tsx
@@ -0,0 +1,21 @@
+import './index.css';
+import { Component } from "react";
+export default class NewWebsiteComponent extends Component {
+    render() {
+        return (
+            <form action = "/new-website">
+                <h3>Add a new website</h3>
+                <div className="form-group">
+                    <label>Website name</label>
+                    <input type="text" id="websiteName" className="form-control" placeholder="Enter name" required />
+                </div>
+                <div className="form-group">
+                    <label>Website address</label>
+                    <input type="url" id="websiteURL" className="form-control" placeholder="Enter url" required />
+                </div>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" > Add website </button>
+            </form>
+
+        );
+    }
+}
