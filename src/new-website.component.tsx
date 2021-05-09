dbda5a23c0e004f07b93fb178b0b6e526553390c
aksek <kosekaniela@gmail.com>
2021-05-09 09:21:17 +0200

    corrected spelling

--- src/new-website.component.tsx
+++ src/new-website.component.tsx
@@ -4,18 +4,17 @@ export default class NewWebsiteComponent extends Component {
     render() {
         return (
             <form action = "/new-website">
-                <h3>Add a new website</h3>
+                <h3>New website</h3>
                 <div className="form-group">
                     <label>Website name</label>
                     <input type="text" id="websiteName" className="form-control" placeholder="Enter name" required />
                 </div>
                 <div className="form-group">
                     <label>Website address</label>
-                    <input type="url" id="websiteURL" className="form-control" placeholder="Enter url" required />
+                    <input type="url" id="websiteURL" className="form-control" placeholder="Enter URL" required />
                 </div>
                 <button type="submit" className="btn btn-dark btn-lg btn-block" > Add website </button>
             </form>
-
         );
     }
 }
