32eb4b9a4924e408123d23be68789991200700c9
aksek <kosekaniela@gmail.com>
2021-05-10 21:46:39 +0200

    improved layout and simplified code

---
+++ src/website-dashboard.component.tsx
@@ -0,0 +1,29 @@
+import './index.css';
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2"
+import Card from "@material-ui/core/Card"
+import Grid from "@material-ui/core/Grid"
+import EventData from "./event-data"
+
+class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+}
+
+export default class WebsiteDashboardComponent extends Component {
+
+    render() {
+        window.ResizeObserver = ResizeObserver;
+        return (
+            <div>
+                <h3>Dashboard</h3>
+                <div>
+                    <Grid container spacing={3} direction="row" alignItems="stretch">
+                        {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
+                    </Grid>
+                </div> 
+            </div>
+        );
+    }
+}
