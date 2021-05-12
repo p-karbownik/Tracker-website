a010deeb69fd1767d64cb2f24ec4904f2eb39787
aksek <kosekaniela@gmail.com>
2021-05-12 09:05:20 +0200

    added a back button

--- src/website-dashboard.component.tsx
+++ src/website-dashboard.component.tsx
@@ -1,9 +1,11 @@
 import './index.css'
 import './website-dashboard.component.css';
 import React, { Component } from "react";
+import { Link } from "react-router-dom"
 import { Line } from "react-chartjs-2"
 import Card from "@material-ui/core/Card"
 import Grid from "@material-ui/core/Grid"
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 import EventData from "./event-data"
 
 class ResizeObserver {
@@ -18,7 +20,17 @@ export default class WebsiteDashboardComponent extends Component {
         window.ResizeObserver = ResizeObserver;
         return (
             <div>
-                <h3>Dashboard</h3>
+                
+                <div>
+                    <Grid container spacing={6} direction="row" alignItems="stretch">
+                        <Grid item>
+                            <Link to="./login"><ArrowBackIosIcon></ArrowBackIosIcon> </Link>
+                        </Grid>
+                        <Grid item>
+                            <h3>Dashboard</h3>
+                        </Grid>
+                    </Grid>
+                </div> 
                 <div>
                     <Grid container spacing={1} direction="row" alignItems="stretch">
                         {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
