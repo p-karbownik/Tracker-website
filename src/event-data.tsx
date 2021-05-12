a4bca9f680be222f4d7cbee41f611763f959b94b
aksek <kosekaniela@gmail.com>
2021-05-12 08:16:43 +0200

    made the whole window appear on the screen

--- src/event-data.tsx
+++ src/event-data.tsx
@@ -47,4 +47,29 @@ export default [
         }
         ]
     },
+    {
+        labels: [ -5, -4, -3, -2, -1, 0],
+        datasets: [
+        {
+            label: "click register",
+            data: [5, 6, 7, 8, 1, 4],
+            fill: true,
+            backgroundColor: "rgba(126, 200, 227, 0.2)",
+            borderColor: "rgba(126, 200, 227, 1)"
+        }
+        ]
+    },
+    {
+        labels: [ -5, -4, -3, -2, -1, 0],
+        datasets: [
+        {
+            label: "click cancel",
+            data: [3, 2, 4, 6, 1, 2],
+            fill: true,
+            backgroundColor: "rgba(0, 0, 255, 0.2)",
+            borderColor: "rgba(0, 0, 255, 1)"
+        }
+        ]
+    },
+    
 ];
--- src/website-dashboard.component.tsx
+++ src/website-dashboard.component.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './website-dashboard.component.css';
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2"
 import Card from "@material-ui/core/Card"
@@ -19,7 +19,11 @@ export default class WebsiteDashboardComponent extends Component {
             <div>
                 <h3>Dashboard</h3>
                 <div>
-                    <Grid container spacing={3} direction="row" alignItems="stretch">
+                    <Grid container spacing={1} direction="row" alignItems="stretch">
+                        {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
+                        {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
+                        {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
+                        {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
                         {EventData.map((element) => <Grid item> <Card> < Line type="line" data={element} /> </Card> </Grid>)}
                     </Grid>
                 </div> 
